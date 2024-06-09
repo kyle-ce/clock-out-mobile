e16a9ecf541b2db22d5a2e0b7ef501af801e7d8e
kyle-ce <xkcorc@gmail.com>
2024-06-08 23:14:39 -0400

    remove hard coded light theme

--- app/src/main/java/com/example/clockout/MainActivity.kt
+++ app/src/main/java/com/example/clockout/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ClockoutTheme(darkTheme = false) {
+            ClockoutTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     App(modifier = Modifier.padding(innerPadding))
                 }
